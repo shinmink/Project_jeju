4dceb742a48f1dc7912714e19839aba3fa1f1cae
shinmink <sinmk99@hansung.ac.kr>
2025-08-22 11:08:01 +0900

    FEAT : 플롯, 워드 이미지 버튼 연동 완료

--- MiniProject/app.py
+++ MiniProject/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import subprocess
 
 app = Flask(__name__)
@@ -28,6 +28,36 @@ def run_script():
     message = summary_lines[0]
     preview = summary_lines[1:]  # 상위 5개 영상
     return render_template('index.html', message=message, preview=preview)
+
+# 워드클라우드
+@app.route('/keyword_extractor', methods=['POST'])
+def generate_wordcloud():
+    try:
+        result = subprocess.run(
+            ['python3', 'scripts/keyword_extractor.py'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        image_path = url_for('static', filename='wordcloud.png')
+        return render_template('index.html', image=image_path, message="✅ 워드클라우드 생성 완료!", show_modal=True)
+    except subprocess.CalledProcessError as e:
+        return render_template('index.html', message=f"❌ 워드클라우드 생성 오류:\n{e.stderr}")
+
+# 스캐터 플롯
+@app.route('/scatter_plot', methods=['POST'])
+def generate_scatter():
+    try:
+        result = subprocess.run(
+            ['python3', 'scripts/scatter_plot.py'],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        image_path = url_for('static', filename='scatter.png')
+        return render_template('index.html', image=image_path, message="✅ 스캐터 플롯 생성 완료!", show_modal=True)
+    except subprocess.CalledProcessError as e:
+        return render_template('index.html', message=f"❌ 스캐터 플롯 생성 오류:\n{e.stderr}")
 
 if __name__ == '__main__':
     app.run(debug=True)
